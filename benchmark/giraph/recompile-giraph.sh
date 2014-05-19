624f11b4e487885d4d830cc67ea170b16fda017a
Young Han <young.han@uwaterloo.ca>
2014-05-19 13:50:50 -0400

    Add cscope and Eclipse excludes for rat check.

--- benchmark/giraph/recompile-giraph.sh
+++ benchmark/giraph/recompile-giraph.sh
@@ -8,7 +8,7 @@ cd "$GIRAPH_DIR"
 
 # -pl specifies what packages to compile (e.g., giraph-examples,giraph-core)
 # -Dfindbugs.skip skips "find bugs" stage (saves quite a bit of time)
-mvn clean install -Phadoop_1 -Dhadoop.version=1.0.4 -DskipTests -pl giraph-examples -Dfindbugs.skip
+mvn clean install -Phadoop_1 -Dhadoop.version=1.0.4 -DskipTests -pl giraph-examples,giraph-core -Dfindbugs.skip
 
 # copy compiled jars to worker machines
 for ((i = 1; i <= ${NUM_MACHINES}; i++)); do
