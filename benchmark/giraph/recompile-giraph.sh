f3afaf1199e906760f9884e0d3086e42f3d230d5
Young Han <young.han@uwaterloo.ca>
2014-05-22 19:22:03 -0400

    Enable giraph-core compile with arg.

--- benchmark/giraph/recompile-giraph.sh
+++ benchmark/giraph/recompile-giraph.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+# usage: ./recompile-giraph.sh [giraph-core?]
+#
+# giraph-core: 1 to compile giraph-core as well
+
 commondir=$(dirname "${BASH_SOURCE[0]}")/../common
 source "$commondir"/get-hosts.sh
 source "$commondir"/get-dirs.sh
@@ -8,7 +12,11 @@ cd "$GIRAPH_DIR"
 
 # -pl specifies what packages to compile (e.g., giraph-examples,giraph-core)
 # -Dfindbugs.skip skips "find bugs" stage (saves quite a bit of time)
-mvn clean install -Phadoop_1 -Dhadoop.version=1.0.4 -DskipTests -pl giraph-examples,giraph-core -Dfindbugs.skip
+if [ $1 -eq 1 ]; then
+    mvn clean install -Phadoop_1 -Dhadoop.version=1.0.4 -DskipTests -pl giraph-examples -Dfindbugs.skip
+else
+    mvn clean install -Phadoop_1 -Dhadoop.version=1.0.4 -DskipTests -pl giraph-examples,giraph-core -Dfindbugs.skip
+fi
 
 # copy compiled jars to worker machines
 for ((i = 1; i <= ${NUM_MACHINES}; i++)); do
